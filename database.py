84d2ca7b3a223cc9c3fcac7e0ecf9d3da6a3f7cf
TrieuTienThanh <trieutienthanhmmd@gmail.com>
2025-11-28 13:07:05 +0700

    Update

--- database.py
+++ database.py
@@ -1,3 +1,7 @@
 import mysql.connector
 db = mysql.connector.connect(user = 'root', password = '123456789', host = 'localhost', database = 'database')
-cursor = db.cursor()
+#query
+code = 'create database `test`'
+#run
+mycursor = db.cursor()
+mycursor.execute(code)

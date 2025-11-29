0a972d6253349c9198fb65e9c6a2980c4daf065d
TrieuTienThanh <trieutienthanhmmd@gmail.com>
2025-11-29 17:48:52 +0700

    Update

---
+++ em_interface.py
@@ -0,0 +1,9 @@
+from tkinter import *
+root = Tk()
+root.title("Quản lý nhân viên")
+root.minsize(height = 600, width = 1000)
+
+Label(root, text = 'Quản lý nhân viên', fg = 'red', font = ('Cambria', 20), width = 63).grid(row = 0)
+
+
+root.mainloop()
